5a62f43a54ff87bcdb02f5ee2edf82033a7c6bbc
evitla <s.dinmuhamed@gmail.com>
2021-10-13 21:52:44 +0600

    feat: add Header component with style

---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Div } from './style';
+
+const Header = () => {
+  return <Div>Header component</Div>;
+};
+
+export default Header;
---
+++ components/Header/style.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const Div = styled.div`
+  background-color: yellow;
+`;
