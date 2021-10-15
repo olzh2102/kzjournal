6dde5d36222d5ef035dbc4cd75ea436bce166384
Olzhas Kurikov <okur@hellofresh.com>
2021-10-15 08:27:55 +0200

    (O7NCf5aG) setup testing env with sample

---
+++ test/test-utils.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+
+function render(
+    ui,
+    options = {}
+) {
+    function Wrapper({ children }) {
+        return children
+    }
+    return rtlRender(ui, { wrapper: Wrapper, ...options });
+}
+
+export * from '@testing-library/react';
+export { render };
