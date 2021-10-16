2014a12bc39bf2d6672f167f0b668c2f93ae5ed6
Olzhas Kurikov <okur@hellofresh.com>
2021-10-16 18:10:33 +0200

    (dSTRiiJg) // reset margin and padding for body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Global, css } from '@emotion/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <> 
+      <Global 
+        styles={css`
+          body {
+            padding: 0 !important;
+            margin: 0 !important;
+          }
+        `} 
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
