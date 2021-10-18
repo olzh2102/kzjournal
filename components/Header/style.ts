a98079a653ed4e31c7121a83a298730990dd85fd
evitla <s.dinmuhamed@gmail.com>
2021-10-18 15:41:34 +0600

    refactor: move AppBar, Button styled components to style.ts

--- components/Header/style.ts
+++ components/Header/style.ts
@@ -1,5 +1,19 @@
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
 import styled from '@emotion/styled';
 
-export const Div = styled.div`
-  background-color: yellow;
+export const AppBarStyled = styled(AppBar)`
+  background-color: #313e50;
+  position: static;
+`;
+
+export const ButtonStyled = styled(Button)`
+  background-color: #ebeaec;
+  color: #313e50;
+
+  &:hover {
+    background-color: #ebeaec;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15), 0 4px 7px rgba(0, 0, 0, 0.05),
+      0 -1px 0 rgba(0, 0, 0, 0.05), -1px 0 0 rgba(0, 0, 0, 0.05), 1px 0 0 rgba(0, 0, 0, 0.05);
+  }
 `;
