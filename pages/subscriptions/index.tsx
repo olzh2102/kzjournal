704ddcec501712a27c75f05c94fdd586489c444e
Dinmukhamed Sailaubek <57768070+evitla@users.noreply.github.com>
2021-11-06 22:03:21 +0600

    (rOeBReXZ) add raw profile page tabs

---
+++ pages/subscriptions/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import { Box } from '@mui/material';
+
+import WithoutComments from '../../layout/without-comments';
+
+const ProfilePage = () => {
+  return (
+    <>
+      <Head>
+        <title>KZJournal</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Head>
+        <meta property="og:title" content="My new title" key="title" />
+      </Head>
+      <Box>Subscriptions</Box>
+    </>
+  );
+};
+
+ProfilePage.PageLayout = WithoutComments;
+export default ProfilePage;
