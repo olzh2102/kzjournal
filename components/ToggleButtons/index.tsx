8831b9ffa1fda1f199362bd651a620c65fb7e2a1
evitla <s.dinmuhamed@gmail.com>
2021-11-06 22:02:00 +0600

    feat: add raw profile page tabs

--- components/ToggleButtons/index.tsx
+++ components/ToggleButtons/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useState } from 'react';
 
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
@@ -21,26 +22,36 @@ const ColorToggleButton = () => {
       value={alignment}
       exclusive
       onChange={handleChange}
-      sx={{ gap: '16px', mb: 4, justifyContent: 'center' }}
-    >
-      <StyledToggleButton value="popular" size="small" active={alignment === 'popular'}>
-        <WhatshotIcon />
-        Popular
-      </StyledToggleButton>
-      <StyledToggleButton value="latest" size="small" active={alignment === 'latest'}>
-        <AccessTimeIcon />
-        Latest
-      </StyledToggleButton>
-      <StyledToggleButton value="rating" size="small" active={alignment === 'rating'}>
-        <CallMissedOutgoingIcon />
-        Rating
-      </StyledToggleButton>
-      <StyledToggleButton value="subscriptions" size="small" active={alignment === 'subscriptions'}>
-        <FormatListBulletedIcon />
-        Subscriptions
-      </StyledToggleButton>
+      sx={{ gap: '16px', mb: 4, justifyContent: 'center' }}>
+      <Link href="/">
+        <StyledToggleButton value="popular" size="small" active={alignment === 'popular'}>
+          <WhatshotIcon />
+          Popular
+        </StyledToggleButton>
+      </Link>
+      <Link href="/latest">
+        <StyledToggleButton value="latest" size="small" active={alignment === 'latest'}>
+          <AccessTimeIcon />
+          Latest
+        </StyledToggleButton>
+      </Link>
+      <Link href="/rating">
+        <StyledToggleButton value="rating" size="small" active={alignment === 'rating'}>
+          <CallMissedOutgoingIcon />
+          Rating
+        </StyledToggleButton>
+      </Link>
+      <Link href="/subscriptions">
+        <StyledToggleButton
+          value="subscriptions"
+          size="small"
+          active={alignment === 'subscriptions'}>
+          <FormatListBulletedIcon />
+          Subscriptions
+        </StyledToggleButton>
+      </Link>
     </ToggleButtonGroup>
   );
-}
+};
 
 export default ColorToggleButton;
---
+++ pages/latest/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import { Box } from '@mui/material';
+
+import WithoutComments from '../../layout/without-comments';
+
+const ProfilePage = () => {
+  return (
+    <>
+      <Head>
+        <title>KZJournal</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Head>
+        <meta property="og:title" content="My new title" key="title" />
+      </Head>
+      <Box>Latest</Box>
+    </>
+  );
+};
+
+ProfilePage.PageLayout = WithoutComments;
+export default ProfilePage;
