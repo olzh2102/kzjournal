1219baef21e565ac5c2ff04e07158b5ca985fe19
Olzhas Kurikov <okur@hellofresh.com>
2021-11-11 08:08:35 +0100

    feat: draft components for profile top component

--- layout/without-comments.tsx
+++ layout/without-comments.tsx
@@ -1,7 +1,7 @@
 import Container from '@mui/material/Container';
 
 const WithoutCommentsLayout = ({ children }: { children: React.ReactNode }) => (
-    <Container fixed>
+    <Container fixed style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {children}
     </Container>
 );
--- pages/profile/style.ts
+++ pages/profile/style.ts
@@ -2,17 +2,18 @@ import styled from '@emotion/styled';
 import Paper from '@mui/material/Paper';
 
 export const PaperStyled = styled(Paper)`
-    max-width: 800px;
-    margin-top: 40px;
+    min-width: 700px;
+    margin: 40px;
     padding: 20px;
-    border-radius: 8px;
+    border-radius: 20px;
     border: 1px solid black;
     display: flex;
     height: 300px;
     gap: 16px;
+
     & > div:first-of-type {
         width: 150px;
         height: 150px;
-        border-radius: 8px;
+        border-radius: 20px;
     }
 `
