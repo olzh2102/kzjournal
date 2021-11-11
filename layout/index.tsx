10fcd2d20d663551d3c1210d3787c7f913695222
evitla <s.dinmuhamed@gmail.com>
2021-11-11 22:36:49 +0600

    feat: add page transition

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,5 +1,13 @@
+import { motion } from 'framer-motion';
+
 import Grid from '@mui/material/Grid';
 import SideComments from 'components/SideComments';
+
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => (
   <Grid container spacing={3} style={{ maxWidth: '1200px', margin: '0 auto' }}>
@@ -11,7 +19,19 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => (
       item
       xs={6}
       style={{ paddingLeft: 0, display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      {children}
+      <motion.main
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ type: 'linear' }}
+        className="
+                    flex flex-col items-start w-full pt-10
+                    px-8 sm:px-16 md:px-36 lg:px-52 xl:px-80 2xl:px-96
+                    pt-24 h-full
+                ">
+        {children}
+      </motion.main>
     </Grid>
 
     <Grid item xs>
--- layout/without-comments.tsx
+++ layout/without-comments.tsx
@@ -1,9 +1,29 @@
+import { motion } from 'framer-motion';
+
 import Container from '@mui/material/Container';
 
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
+
 const WithoutCommentsLayout = ({ children }: { children: React.ReactNode }) => (
-    <Container fixed>
-        {children}
-    </Container>
+  <Container fixed>
+    <motion.main
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      transition={{ type: 'linear' }}
+      className="
+                    flex flex-col items-start w-full pt-10
+                    px-8 sm:px-16 md:px-36 lg:px-52 xl:px-80 2xl:px-96
+                    pt-24 h-full
+                ">
+      {children}
+    </motion.main>
+  </Container>
 );
 
 export default WithoutCommentsLayout;
