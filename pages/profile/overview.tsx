bf4778c685afa03880930083d3fe6df987581c12
Olzhas Kurikov <okur@hellofresh.com>
2021-11-11 08:44:25 +0100

    feat: align boxes inside the component

--- pages/profile/overview.tsx
+++ pages/profile/overview.tsx
@@ -8,13 +8,24 @@ import { PaperStyled } from './style'
 const Overview = () => {
     return (
         <PaperStyled variant="outlined">
-            <Image className="image" src="https://leonardo.osnova.io/18c6cdd0-ea0e-536a-a98b-1db21c6c4d2a/-/scale_crop/300x300/-/format/webp/" height={500} width={500}  />
-            <Box>
-                <Typography variant="h5">Yevgeniy Chebatkov</Typography>
-                <Typography variant="subtitle2">With us since 17th of October, 2021</Typography>
+            <Box className="name">
+                <Image layout="intrinsic" className="image" src="https://leonardo.osnova.io/18c6cdd0-ea0e-536a-a98b-1db21c6c4d2a/-/scale_crop/300x300/-/format/webp/" height={500} width={500}  />
+                <Box>
+                    <Typography variant="h5">Yevgeniy Chebatkov</Typography>
+                    <Typography variant="subtitle2">With us since 17th of October, 2021</Typography>
+                </Box>
+                <Box className="cta-buttons">
+                    <Button variant="outlined" size="small">
+                        Settings
+                    </Button>
+                    <Button variant="contained" size="small">
+                        Post
+                    </Button>
+                </Box>
             </Box>
 
-            <Box>
+
+            <Box className="links">
                 <IconButton aria-label="delete" size="small">
                     <MessageOutlinedIcon fontSize="inherit" />
                 </IconButton>
@@ -31,14 +42,8 @@ const Overview = () => {
                     <MessageOutlinedIcon fontSize="inherit" />
                 </IconButton>             
             </Box>
-            <Box>
-                <Button variant="outlined" size="small">
-                    Settings
-                </Button>
-                <Button variant="contained" size="small">
-                    Post
-                </Button>
-            </Box>
+
+            
         </PaperStyled>
     )
 }
--- pages/profile/style.ts
+++ pages/profile/style.ts
@@ -5,15 +5,30 @@ export const PaperStyled = styled(Paper)`
     min-width: 700px;
     margin: 40px;
     padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
     border-radius: 20px;
     border: 1px solid black;
     display: flex;
     height: 300px;
     gap: 16px;
 
-    & > div:first-of-type {
-        width: 150px;
-        height: 150px;
-        border-radius: 20px;
+    .name {
+        display: flex;
+        gap: 16px;
+
+        & > div:first-of-type {
+            width: 150px;
+            height: 150px;
+            border-radius: 20px;
+        }
+
+        .cta-buttons {
+            margin-left: auto;
+            > button {
+                margin-right: 10px;    
+            }
+        }
     }
 `
